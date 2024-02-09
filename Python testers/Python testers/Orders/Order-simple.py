cf0db1f275950fdf2f4145b0649cc07b56503328
Yassine Raouz <44752439+Yassinator10@users.noreply.github.com>
2024-02-09 12:14:40 -0500

    Update Order-simple.py

--- Python testers/Python testers/Orders/Order-simple.py
+++ Python testers/Python testers/Orders/Order-simple.py
@@ -13,42 +13,37 @@ class TestApp(EClient, EWrapper):
     def nextValidId(self, orderId: OrderId):
 
         mycontract = Contract()
-        #mycontract.exchange = "SMART"
-        mycontract.conId = 265598
+        
+        mycontract.conId = 130346379
        
         mycontract.exchange = "SMART"
         #mycontract.currency = "USD"
 
         
-        #mycontract.primaryExchange = "ISLAND"
-        # mycontract.localSymbol = "ESH3"
-        # mycontract.tradingClass = "SPXW"
-        # mycontract.lastTradeDateOrContractMonth = "20230202"
-        # mycontract.right = "P"
-        # mycontract.strike = 4150
+        
 
         myorder = Order()
         myorder.action = "BUY"
-        myorder.totalQuantity = 1
+        myorder.totalQuantity = 200
         myorder.orderType = "MKT"
         myorder.tif = "DAY"
         #myorder.duration = ""
         #myorder.auxPrice= 160
-        #myorder.goodTillDate = "20230713 15:15: FUCKKK"
+        #myorder.goodTillDate = "20230713 15:15:"
         #myorder.auxPrice = 250
         #myorder.lmtPrice = 575.6
         #myorder.whatIf = True
         #myorder.cashQty = 100000
         #myorder.notHeld = True
-        myorder.transmit = True
+        #myorder.transmit = True
 
         self.placeOrder(orderId, mycontract, myorder)
 
     def openOrder(self, orderId: OrderId, contract: Contract, order: Order, orderState: OrderState):
         print("openOrder.", orderId, contract, order, orderState)
 
-    # def orderStatus(self, orderId: OrderId, status: str, filled: Decimal, remaining: Decimal, avgFillPrice: float, permId: int, parentId: int, lastFillPrice: float, clientId: int, whyHeld: str, mktCapPrice: float):
-    #     print("orderStatus", "datetime: ",datetime.now(), orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice)
+    def orderStatus(self, orderId: OrderId, status: str, filled: Decimal, remaining: Decimal, avgFillPrice: float, permId: int, parentId: int, lastFillPrice: float, clientId: int, whyHeld: str, mktCapPrice: float):
+        print("orderStatus", "datetime: ",datetime.now(), orderId, status, filled, remaining, avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice)
     
 
 app = TestApp()
