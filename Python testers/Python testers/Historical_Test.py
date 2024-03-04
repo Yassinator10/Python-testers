f850daccc62a8a4e7e8dfa5d3454e4229592dd79
Yassine Raouz <44752439+Yassinator10@users.noreply.github.com>
2024-03-04 11:00:42 -0500

    Add files via upload

---
+++ Python testers/Python testers/Historical_Test.py
@@ -0,0 +1,88 @@
+from socket import timeout
+from symtable import Symbol
+from ibapi.client import *
+from ibapi.wrapper import *
+import datetime
+from ibapi.tag_value import TagValue
+from ibapi.contract import ComboLeg
+import threading
+datetime.datetime.now()
+port = 7496
+
+
+class TestApp(EClient, EWrapper):
+    def __init__(self):
+        EClient.__init__(self, self)
+
+    def nextValidId(self, orderId: OrderId):
+        
+        mycontract = Contract()
+        mycontract.exchange = "KSE"
+        #mycontract.symbol = "SPX"
+        mycontract.conId = 655454315
+        #mycontract.currency = "USD"
+        #mycontract.secType = "IND"
+        #mycontract.symbol = "INDU"
+        #mycontract.secType = "IND"
+        #mycontract.currency = "USD"
+        
+        #mycontract.localSymbol="SPX   230721C04520000"
+        #mycontract.primaryExchange = "NASDAQ"
+        #mycontract.multiplier = 100
+        #mycontract.primaryExchange = "ARCA"
+        #mycontract.lastTradeDateOrContractMonth = "202310"
+        #mycontract.strike = 4370
+        #mycontract.right = "P"
+        #mycontract.tradingClass = "SPX"
+
+        
+        threading.Thread(target=self.reqHistoricalData(
+            reqId=123,
+            contract=mycontract,
+            endDateTime="",
+            #endDateTime="",
+            #endDateTime=datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"),
+            durationStr= "2 D",
+            barSizeSetting = "1 min",
+            whatToShow= "MIDPOINT",#"TRADES",
+            useRTH=0,
+            formatDate=1,
+            keepUpToDate=True,
+            chartOptions=[],
+        )).start()
+        '''
+        threading.Thread(self.reqHistoricalData(
+            reqId=456,
+            contract=mycontract,
+            # endDateTime="20230208 10:47:01 US/Eastern",
+            endDateTime="",
+            # endDateTime=datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"),
+            durationStr= "3600 S",
+            barSizeSetting = "15 mins",
+            whatToShow= "TRADES",
+            useRTH=1,
+            formatDate=1,
+            keepUpToDate=True,
+            chartOptions=[],
+        )).start()
+    '''
+    def historicalDataUpdate(self, reqId: int, bar: BarData):
+        print("histUpdate.", reqId, bar)
+
+    def historicalData(self, reqId: int, bar: BarData):
+        print(reqId, bar)
+        # print(bar.date)
+
+
+    def historicalDataEnd(self, reqId: int, start: str, end: str):
+        print(reqId, start, end)
+        self.disconnect()
+
+    def error(self, reqId: TickerId, errorCode: int, errorString: str, advancedOrderRejectJson=""):
+        print(reqId, errorCode, errorString, advancedOrderRejectJson)
+
+
+
+app = TestApp()
+app.connect("127.0.0.1", port, 1001)
+app.run()
